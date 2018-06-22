4b54d89fdcb9c937fe948b7ba1521e4e695f79c8
Sarah Li <sarcl@hotmail.com>
2018-06-22 13:00:11 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,6 +1,11 @@
 
 def create_olympics_hash
   # Implement this method so that it returns a hash with the data provided on README.md
+  summer_olympics  = {:Sydney=>2000, :Athens=>2004, :Beijing=>2008,:London=>2012
+}
+summer_olympics["Atlanta"]
+create_olympics_hash.each do|key,value|
+
 end
 
 def add_a_key_value_pair
